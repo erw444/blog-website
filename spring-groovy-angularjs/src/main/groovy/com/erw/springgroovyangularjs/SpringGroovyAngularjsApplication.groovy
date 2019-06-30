77af13292ae371c89c5b68d25f9e1ab59d58c966
Ethan Whittemore <erw444@gmail.com>
2019-06-30 13:08:59 -0500

    first commit

---
+++ spring-groovy-angularjs/src/main/groovy/com/erw/springgroovyangularjs/SpringGroovyAngularjsApplication.groovy
@@ -0,0 +1,13 @@
+package com.erw.springgroovyangularjs
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringGroovyAngularjsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SpringGroovyAngularjsApplication, args)
+	}
+
+}
---
+++ src/main/groovy/com/erw/blogwebsite/BlogWebsiteApplication.groovy
@@ -0,0 +1,13 @@
+package com.erw.blogwebsite
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BlogWebsiteApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(BlogWebsiteApplication, args)
+	}
+
+}
